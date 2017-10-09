4b75a09c12ebe82b3183e1cf7491c0a297695886
Raúl Uranga <ruranga@gmail.com>
2017-10-09 00:58:06 -0500

    step(s05): local web server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function () {
+      browserSync.init({
+          server: {
+              baseDir: "public/",
+              index: "london.html"
+          }
+      });
+});
 
 gulp.task('views', function buildHTML() {
   return gulp.src('views/*.pug')
@@ -9,7 +19,7 @@ gulp.task('views', function buildHTML() {
   .pipe(gulp.dest("public/"))
 });
 
-gulp.task('default', function() {
+gulp.task('default', ["serve"], function() {
   // place code for your default task here
   gulp.watch("views/*.pug", ['views']);
 });
