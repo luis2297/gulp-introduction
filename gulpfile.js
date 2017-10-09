d14817209928bb9f951cf67064312b732eb0c3ec
Raúl Uranga <ruranga@gmail.com>
2017-10-09 00:04:36 -0500

    step(s01): first task.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+
+gulp.task('views', function buildHTML() {
+  return gulp.src('views/*.pug')
+  .pipe(pug({
+    // Your options in here.
+  }))
+  .pipe(gulp.dest("public/"))
+});
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
