eef23752d5219d0385e37c2e617f2aef209f4ef6
Raúl Uranga <ruranga@gmail.com>
2017-10-09 01:30:05 -0500

    step(s06): styles.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('serve', function () {
@@ -19,8 +20,17 @@ gulp.task('views', function buildHTML() {
   .pipe(gulp.dest("public/"))
 });
 
+gulp.task('styles', function () {
+  return gulp.src('./styles/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/'))
+    .pipe(browserSync.stream());
+});
+
 gulp.task('default', ["serve"], function() {
   // place code for your default task here
   gulp.watch("views/*.pug", ['views']);
+  gulp.watch("styles/*.sass", ['styles']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
