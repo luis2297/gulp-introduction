2a6f028d92798cfdd9cb7b1fab204f8268570e70
Raúl Uranga <ruranga@gmail.com>
2017-10-09 12:55:40 -0500

    step(s08): optimize.

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,23 @@ var pug = require('gulp-pug');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var ts = require('gulp-typescript');
+
+var uglifycss = require('gulp-uglifycss');
+var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
+var runSequence = require('run-sequence');
+var isProduction = process.env.NODE_ENV === "production"
+
 var tsProject = ts.createProject('tsconfig.json', {
   typescript: require('typescript')
 });
 
-
 gulp.task('scripts', function() {
   var tsResult = gulp.src("scripts/**/*.ts")
       .pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest('public/'));
+  return tsResult.js
+    .pipe(gulpif(isProduction, uglify()))
+    .pipe(gulp.dest('public/'));
 });
 
 gulp.task('serve', function () {
@@ -26,7 +34,7 @@ gulp.task('serve', function () {
 gulp.task('views', function buildHTML() {
   return gulp.src('views/*.pug')
   .pipe(pug({
-    pretty: true
+    pretty: !isProduction
   }))
   .pipe(gulp.dest("public/"))
 });
@@ -34,12 +42,16 @@ gulp.task('views', function buildHTML() {
 gulp.task('styles', function () {
   return gulp.src('./styles/*.sass')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulpif(isProduction, uglifycss()))
     .pipe(gulp.dest('public/'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('default', ["serve"], function() {
-  // place code for your default task here
+
+gulp.task('default', function() {
+
+  runSequence('scripts', ['styles', 'views'], 'serve');
+
   gulp.watch("views/**/*.pug", ['views']);
   gulp.watch("scripts/**/*.ts", ['scripts']);
   gulp.watch("styles/*.sass", ['styles']);
